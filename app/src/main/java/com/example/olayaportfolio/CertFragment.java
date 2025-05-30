26da68c23724ede989a6dc92426c54e2dc8b97f8
Vvin <mobileappdev.olaya@gmail.com>
2025-05-30 11:34:39 +0800

    Initial Commit (Fixed Certs Fragment)

--- app/src/main/java/com/example/olayaportfolio/CertFragment.java
+++ app/src/main/java/com/example/olayaportfolio/CertFragment.java
@@ -1,6 +1,23 @@
 package com.example.olayaportfolio;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CertFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cert, container, false); // Make sure this matches your XML filename
+    }
 }
+
